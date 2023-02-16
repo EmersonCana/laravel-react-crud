001622aecb57a1a47d52bdaeb4e19a7e1bdb869c
DESKTOP-4AILAGU\PC <emersonc.zeniark@gmail.com>
2023-02-17 00:11:36 +0800

    update cors

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,8 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-          axios.defaults.withCredentials = true;
+        axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+        axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
           await axios.get(url).then(function (response) {
             setProducts(response.data)
           }).catch(function (error) {
